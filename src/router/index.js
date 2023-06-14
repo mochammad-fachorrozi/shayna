a2ae88426ecb29cf0bb58075427d5e3968abf20a
mochammad-fachorrozi <mochammadfachorrozi98@gmail.com>
2023-06-14 14:31:29 +0700

    create shayna project

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ProductView from '../views/ProductView.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
+import Success from '../views/Success.vue'
+
+
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: ProductView
+  },
+  {
+    path: '/cart',
+    name: 'ShoppingCart',
+    component: ShoppingCart
+  },
+  {
+    path: '/success',
+    name: 'SuccessShayna',
+    component: Success
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <HeaderShayna></HeaderShayna>
+    <HeroShayna></HeroShayna>
+    <WomenShayna></WomenShayna>
+    <InstaShayna></InstaShayna>
+    <PartnerShayna></PartnerShayna>
+    <FooterShayna></FooterShayna>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomenShayna from '@/components/WomenShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna,
+  }
+}
+</script>
